9483939eae188c1573096749fcf51a76461641b1
José Granados <jmgranadospy@gmail.com>
2025-04-27 09:57:02 -0500

    Actualizado 5-.py
    Añadido introducción del ejercicio a realizar.

--- ejercicios_semana1/01.1_operadores_aritmeticos/5-.py
+++ ejercicios_semana1/01.1_operadores_aritmeticos/5-.py
@@ -1,3 +1,8 @@
+'''
+### 5. Convertir temperatura (F ↔ C)
+Pide una temperatura en Fahrenheit y muestra el equivalente en Celsius.
+'''
+
 temperaturaIngresada = float(input("Ingrese la temperatura en grados farenheit: "))
 
 temperaturaConvertida = (temperaturaIngresada - 32) * 5/9
